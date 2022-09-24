ab989d8b2afc2cba07ffbd6af9becc56cb288e8b
Marina Chernysheva <marich.99@mail.ru>
2022-09-24 18:43:20 +0300

    Объявила методы и создала первичную проверку, придумала команды

--- File_Manager/main.py
+++ File_Manager/main.py
@@ -1,14 +1,36 @@
 import json
+import inspect
 
 
 class Manager:
+
     def __init__(self):
         # self.path_directory = path
         self.data = self.get_directory()
 
+        self.dict_cmds = {
+            'cdir': self.create_directory,
+            'ddir': self.delete_directory,
+            'mdir': self.move_directory,
+            'cfile': self.create_file,
+            'wfile': self.write_file,
+            'rfile': self.read_file,
+            'dfile': self.delete_file,
+            'cpfile': self.copy_file,
+            'mfile': self.move_file,
+            'rnfile': self.rename_file
+        }
+
     def menu(self):
         while True:
             move = input('Введите действие: ')
+            command, *args = move.split()
+            if command in self.dict_cmds.keys():
+                args_func = inspect.getfullargspec(self.dict_cmds[command])[0]
+                if len(args_func) - 1 == len(args):
+                    self.dict_cmds[command](*args)
+                else:
+                    print(f'Функция {command} ждет {len(args_func) - 1} аргумент(а)')
 
     def get_directory(self):
         with open('settings.json', 'r', encoding='utf-8') as file:
@@ -24,12 +46,40 @@ class Manager:
             json.dump(data_dict, file, indent=4)
         self.data = fr"{new_path}"
 
+    def create_directory(self, path):
+        pass
+
+    def delete_directory(self, path):
+        pass
+
+    def move_directory(self, start_path, finish_path):
+        pass
+
+    def create_file(self, path):
+        pass
+
+    def write_file(self, path, text):
+        pass
+
+    def read_file(self, path):
+        pass
+
+    def delete_file(self, path):
+        pass
+
+    def copy_file(self, start_path, finish_path):
+        pass
+
+    def move_file(self, start_path, finish_path):
+        pass
+
+    def rename_file(self, path, new_name):
+        pass
+
     def __str__(self):
         return f'Путь файла {self.data}'
 
 
 if __name__ == '__main__':
     manager = Manager()
-    print(manager)
-    manager.choose_directory()
-    print(manager)
+    manager.menu()
