7bf4d4b5d4d9bad30d29523600f310f98f184aee
Marina Chernysheva <marich.99@mail.ru>
2022-10-26 01:20:24 +0300

    Добавила методы для записи json-файла

--- echo_server/server.py
+++ echo_server/server.py
@@ -22,6 +22,7 @@ class Server:
         self.clients = []
         self.users = 'users.json'
         self.server_run()
+        self.status = None
 
     def server_run(self):
         """
@@ -53,12 +54,66 @@ class Server:
                 break
 
             if data:
-                pass
+                status, data, username = pickle.loads(data)
+                logging.info(f"Прием данных от клиента '{username}_{address[1]}': {data}")
+                if status == "message":
+                    self.broadcast(data, conn, address, username)
 
-
+            else:
+                # Закрываем соединение
+                conn.close()
+                self.clients.remove(conn)
+                logging.info(f"Отключение клиента {address}!")
+                break
 
     def authorization(self, address, conn):
+        """
+        Авторизация пользователя на сервере
+        :param address: IP-адрес и номер соединения
+        :param conn: сокет
+        """
+        try:
+            self.users_authorization = self.read_json()
+        except json.decoder.JSONDecodeError:
+            self.registration(address, conn)
+
+        registration_user = True
+        for users in self.users_authorization:
+            if address[0] in users:
+                for key, value in users.items():
+                    if key == address[0]:
+                        name_user = value['name']
+                        password_user = value['password']
+                        # дописать
+                        registration_user = False
+        if registration_user:
+            self.registration(address, conn)
+
+    def broadcast(self):
         pass
+
+    def read_json(self):
+        """Чтение файла с авторизованными пользователями"""
+        with (self.users, 'r') as file:
+            users_text = json.load(file)
+        return users_text
+
+    def registration(self, address, conn):
+        """
+        Регистрация новых пользователей
+        и добавление информации о них в json файл
+        Args:
+            :param address: IP-адрес и номер соединения
+            :param conn: сокет
+        """
+        pass
+
+    def write_json(self):
+        """
+        Запись пользователей в json-файл
+        """
+        with open(self.users, 'w', encoding='utf-8') as file:
+            json.dump(self.users_authorization, file, indent=4)
 
 
 def main():
