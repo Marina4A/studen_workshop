5a0c2ff1339f2c37caae983e4ca38c59b05d59f1
Marina Chernysheva <marich.99@mail.ru>
2023-01-08 13:04:05 +0300

    Задала настройки параметрам экрана

---
+++ complex_animation/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+
+    def __init__(self):
+        # Параметры экрана
+        self.screen_width = 870
+        self.screen_height = 700
+        self.bg_color = (0, 0, 0)
+        self.FPS = 50
+
